f6d9ca948b93150be4ff7bb862d59f0df53c672f
Deribew Shimelis wosene <118039173+deribewsoftware@users.noreply.github.com>
2023-11-17 18:53:04 +0300

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,22 @@
-https://leetcode.com/problems/fizz-buzz/submissions/1100870340/
+class Solution(object):
+    def fizzBuzz(self, n):
+        res=[]*n
+        for i in range(1,n+1):
+            if i%3==0 and i %5==0:
+                res.insert(i-1,"FizzBuzz")
+            elif i%3==0:
+                res.insert(i-1,"Fizz")
+            elif i%5==0:
+                res.insert(i-1,"Buzz")
+            else:
+                res.insert(i-1,str(i))
+        return res
+
+
+        
+            
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
