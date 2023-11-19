9579fbbbcbd2cffa4ed3c647a32928ff667a1fba
Deribew Shimelis wosene <118039173+deribewsoftware@users.noreply.github.com>
2023-11-19 11:44:16 +0300

    Update goodPairs.py

--- goodPairs.py
+++ goodPairs.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        res=[]
+
+        for i in range(1,len(nums)):
+            j=0
+            while j<len(nums)-1:
+                if j<i and nums[i]==nums[j]:
+                    res.append((j,i))
+                j+=1
+        return len (res)
+
+
+        
